11f4f5b25587ded6b96757892e20a34e2c207bcb
ddumaa <dimaduma62@gmail.com>
2023-12-20 23:44:29 +0300

    first commit

---
+++ src/homework/task_6/Runner.java
@@ -0,0 +1,21 @@
+package homework.task_6;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Runner {
+    public static void main(String[] args) {
+        Set<String> name = new HashSet<>();
+        name.add("Dmitriy");
+        name.add("Polina");
+        name.add("Mark");
+        name.add("Daniil");
+        name.add("Liisa");
+        name.add("Stepan");
+        name.add("Artur");
+        name.add("Ivan");
+        name.add("Andrey");
+        name.add("nameTG: Strfend");
+        System.out.println(name);
+    }
+}
